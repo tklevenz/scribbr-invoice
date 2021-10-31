ae6a0d83482a57c4afa89c1836aade8c6fea0b82
Tobias <tobias@bizcuit.nl>
2021-10-31 21:02:24 +0100

    add router

--- invoice-app/src/App.js
+++ invoice-app/src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
+import { Router, Route } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+      <Router history={createBrowserHistory({})}>
+        <Route
+          path="/invoices"
+          exact
+        />
+        <Route
+          path="/invoices/:id"
+          exact
+        />
+      </Router>
+    </React.Fragment>
   );
 }
 

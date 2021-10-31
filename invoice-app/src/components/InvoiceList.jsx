eafd925cdd90baae4fdb7d8dcd03c8c68a4fda62
Tobias <tobias@bizcuit.nl>
2021-10-31 22:34:15 +0100

    add material ui List

--- invoice-app/src/components/InvoiceList.jsx
+++ invoice-app/src/components/InvoiceList.jsx
@@ -1,21 +1,48 @@
 import React, { useContext, useEffect } from 'react';
 import { observer, MobXProviderContext } from 'mobx-react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import DescriptionIcon from '@mui/icons-material/Description';
 
 const InvoiceList = observer(() => {
   const { invoiceStore } = useContext(MobXProviderContext);
 
   useEffect(() => {
+    if (invoiceStore.invoices.size) return;
     invoiceStore.loadInvoices();
   }, [invoiceStore])
 
   return (
     <React.Fragment>
-      <div>Invoice List</div>
-      { invoiceStore.invoices.size && [...invoiceStore.invoices.values()].map(invoice => (
-        <div key={invoice.id}>
-          <p>{invoice.description}</p>
-        </div>
-      ))}
+      <List
+        dense
+        disablePadding
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        { invoiceStore.invoices.size && [...invoiceStore.invoices.values()].map(invoice => (
+          <ListItem
+            key={invoice.id}
+            style={{ maxWidth: '1040px' }}
+          >
+            <ListItemAvatar>
+              <Avatar>
+                <DescriptionIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemButton>
+              <ListItemText primary={invoice.description} secondary={invoice.total} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
     </React.Fragment>
   )
 });
